edafcb49eb7eff1bef549b5d74aca837a5d1df0b
ondren <andreykl123321@gmail.com>
2024-09-28 17:50:59 +0300

    added .env + API response interface

---
+++ parma_project/src/shared/types.ts
@@ -0,0 +1,16 @@
+export interface IDrills {
+    name: string,
+    diameter: number,
+    length: number,
+    deep_of_drill: number,
+    plate: string,
+    screws: string,
+    key: string,
+    company: string,
+    is_broken: boolean,
+    image_path: string,
+    storage: string,
+    create_at: string,
+    update_at: string,
+    id: number
+}
