d13fe26db2e368d8883a538f508350ddc58a1edf
Vladislavgolovin95 <vladislav.golovin95@mail.ru>
2024-09-29 21:11:39 +0300

    added env and endpoint

--- parma_project/src/widgets/api.ts
+++ parma_project/src/widgets/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { BASE_URL } from "../shared/consts/consts.ts";
+import { endpoints } from "../shared/consts/consts";
 
 export const getData = async () => {
   try {
-    const res = await axios.get(BASE_URL);
+    const res = await axios.get(`${import.meta.env.VITE_BASE_URL}${endpoints.drills}`);
       return res.data
   } catch (error) {
       console.log(error)
